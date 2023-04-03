8dbcc43d937f8d571f98a45fad46945953aa3125
JM <1910598john@gmail.com>
2023-04-03 12:09:54 +0800

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,55 +4,70 @@ import 'package:taskreminder/homescreen.dart';
 import 'package:taskreminder/set_alarm.dart';
 import 'gender.dart';
 import 'db_helper.dart';
-import 'package:workmanager/workmanager.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-/*
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   _MyApp createState() => _MyApp();
 }
 
 class _MyApp extends State<MyApp> {
-*/
-  /*
-  FlutterLocalNotificationsPlugin localNotification =
-      FlutterLocalNotificationsPlugin();
-
+  FlutterLocalNotificationsPlugin? flutterlocalNotificationPlugin;
   @override
   void initState() {
     super.initState();
-    var androidInitialize = new AndroidInitializationSettings('flutter_logo');
-    var initializationSettings =
-        new InitializationSettings(android: androidInitialize);
-    localNotification = new FlutterLocalNotificationsPlugin();
-    localNotification.initialize(initializationSettings);
+    var initializationSettingsAndroid =
+        new AndroidInitializationSettings('@mipmap/ic_launcher');
+    var initializationSettingsIOS = new IOSInitializationSettings();
+    var initializationSettings = new InitializationSettings(
+        android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+    flutterlocalNotificationPlugin = new FlutterLocalNotificationsPlugin();
+    flutterlocalNotificationPlugin!.initialize(initializationSettings);
   }
 
-  Future _showNotification() async {
-    var androidDetails = new AndroidNotificationDetails(
-        'channelId', 'Local Notification', 'HOPE IT WORKS',
-        importance: Importance.high);
-    var generalNotificationDetails =
-        new NotificationDetails(android: androidDetails);
-    await localNotification.show(
-        0, "TITLE", "TEST CONTENT", generalNotificationDetails);
-  }
-  */
   @override
   Widget build(BuildContext context) {
-    //Workmanager().initialize(test);
-    return MaterialApp(
-      theme: ThemeData(
-        splashColor: Colors.transparent,
-        highlightColor: Colors.transparent,
-        hoverColor: Colors.transparent,
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Plugin example app'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              TextButton(
+                onPressed: _showNotificationWithSound,
+                child: new Text('Show Notification With Sound'),
+              ),
+            ],
+          ),
+        ),
       ),
-      home: CheckGender(),
-      debugShowCheckedModeBanner: false,
     );
+  }
+
+  Future _showNotificationWithSound() async {
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        'channelId', 'channelName', 'channelDescription',
+        importance: Importance.max,
+        priority: Priority.max,
+        fullScreenIntent: true);
+    var iosPlatformChannelSpecifics = IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+      android: androidPlatformChannelSpecifics,
+      iOS: iosPlatformChannelSpecifics,
+    );
+
+    await flutterlocalNotificationPlugin!.show(
+        0, 'TITLE', 'body', platformChannelSpecifics,
+        payload: 'Default_Sound');
   }
 }
